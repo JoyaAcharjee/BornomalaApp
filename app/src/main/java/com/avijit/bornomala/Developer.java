46073b139ccddd5d6dc44845afb68cfa289811ca
JoyaAcharjee <joyaacharjee090@gmail.com>
2020-07-28 09:22:19 -0700

    Updated

--- app/src/main/java/com/avijit/bornomala/Developer.java
+++ app/src/main/java/com/avijit/bornomala/Developer.java
@@ -1,4 +1,15 @@
 package com.avijit.bornomala;
 
-public class Developer {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Developer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soroborno);
+    }
+
 }
